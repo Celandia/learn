a5e65d874096cc1845eebf23c80ddfc296630e6a
chengyan9 <chengyan9@jd.com>
2019-04-29 17:07:54 +0800

    新增

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
 import './index.scss';
+
+//  es6语法 处理异步  async  await  promise
+
+
+async function timeOut() {
+    return 'hello world'
+}
+console.log(timeOut())
+
+timeOut().then(result => {
+    console.log(result)
+})
+
+console.log('我是后来居上')
+
+
+
+function doubleAfter2seconds(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(2 * num)
+        }, 2000)
+    })
+}
+
+
+async function testResult() {
+    let result = await doubleAfter2seconds(10);
+    console.log(result)
+}
+
+testResult();
